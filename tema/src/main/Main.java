18b4a6310aba5707529d5d030423db79aaa06acb
claudiububatu <73953181+claudiububatu@users.noreply.github.com>
2021-11-30 22:58:09 +0200

    commit 1

--- tema/src/main/Main.java
+++ tema/src/main/Main.java
@@ -70,7 +70,10 @@ public final class Main {
         Writer fileWriter = new Writer(filePath2);
         JSONArray arrayResult = new JSONArray();
 
+        System.out.println("cevaaa");
         //TODO add here the entry point to your implementation
+        System.out.println("cevaaa");
+
 
         fileWriter.closeJSON(arrayResult);
     }
